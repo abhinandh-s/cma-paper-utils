440e28105b093eb3e710dd593f3dd3dd6e31c563
abhinandh-s <168844210+abhinandh-s@users.noreply.github.com>
2026-07-15 07:12:40 +0000

    chore: compile rust to wasm [skip ci]

--- lib/rs_lib.d.ts
+++ lib/rs_lib.d.ts
@@ -2,6 +2,7 @@
 // deno-lint-ignore-file
 // deno-fmt-ignore-file
 
+export function paperIdToLevel(id: string): string;
 export function add(a: number, b: number): number;
 export class Greeter {
   free(): void;
